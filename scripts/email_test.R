a705cc2d251f685a47b63161573a209c26ecbe88
Kyle Lima <klima@schoodicinstitute.org>
2023-01-21 16:29:28 -0500

    quick email test

--- scripts/email_test.R
+++ scripts/email_test.R
@@ -6,19 +6,17 @@ my_email_object <- render_email('outputs/emailtest.Rmd')
 print(my_email_object)
 
 # create_smtp_creds_key(
-#   id = "gmail",
+#   id = "kmail",
 #   user = "klima@schoodicinstitute.org",
-#   host = "smtp.gmail.com",
-#   port = 465,
-#   use_ssl = TRUE,
-#   overwrite = TRUE
+#   provider = "gmail",
+#   #overwrite = TRUE
 # )
 
 smtp_send(my_email_object,
           from = "klima@schoodicinstitute.org",
           to = "klima@schoodicinstitute.org",
-          subject = "Your email subject",
-          credentials = creds_key("gmail")
+          subject = "test email",
+          credentials = creds_key("kmail")
 )
 
 
